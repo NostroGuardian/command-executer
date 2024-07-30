8c447440eee4f96c1c05614e142669c3f370e973
nostroguardian <shmelivan5@yandex.ru>
2024-07-30 16:49:36 +0300

    Final Fixes

--- dist/commands/ffmpeg/ffmpeg.executor.js
+++ dist/commands/ffmpeg/ffmpeg.executor.js
@@ -10,9 +10,12 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.FfmpegExecutor = void 0;
+const child_process_1 = require("child_process");
 const command_executor_1 = require("../../core/executor/command.executor");
 const file_service_1 = require("../../core/files/file.service");
 const prompt_service_1 = require("../../core/prompt/prompt.service");
+const ffmpeg_builder_1 = require("./ffmpeg.builder");
+const stream_handler_1 = require("../../core/handlers/stream.handler");
 class FfmpegExecutor extends command_executor_1.CommandExecutor {
     constructor(logger) {
         super(logger);
@@ -28,14 +31,21 @@ class FfmpegExecutor extends command_executor_1.CommandExecutor {
             return { width, height, path, name };
         });
     }
-    build(input) {
-        throw new Error('Method not implemented.');
+    build({ width, height, path, name }) {
+        const output = this.fileService.getFilePath(path, name, 'mp4');
+        const args = new ffmpeg_builder_1.FfmpegBuilder()
+            .input(path)
+            .setVideoSize(width, height)
+            .output(output);
+        return { command: 'ffmpeg', args, output };
     }
-    spawn(command) {
-        throw new Error('Method not implemented.');
+    spawn({ output, command, args, }) {
+        this.fileService.deleteFileIfExists(output);
+        return (0, child_process_1.spawn)(command, args);
     }
     processStream(stream, logger) {
-        throw new Error('Method not implemented.');
+        const handler = new stream_handler_1.StreamHandler(logger);
+        handler.processOutput(stream);
     }
 }
 exports.FfmpegExecutor = FfmpegExecutor;
